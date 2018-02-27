7a1cdaa5aedd27e15a65c5de1239e06fc2e5c8c4
Barthélémy Launet <blaunet@student.ethz.ch>
2018-02-27 10:55:13 +0100

    - Adding some comments - Defining default to SDSS dataset z=0.1 - Default value for core_path to where the file is stored

--- config.py
+++ config.py
@@ -1,56 +1,62 @@
+import os
 
 class Config:
-    # data selection parameters
-    redshift = '1.25'
-    filter_ = 'h'
+    ## data selection parameters
+    redshift = '0.1'
+    filter_ = 'r'
 
-    # normalization parameters
+    ## normalization parameters
     stretch_type = 'asinh'
     scale_factor = 50
-    
-    # model parameters
+
+    ## model parameters
     learning_rate = 0.00009
     attention_parameter = 0.05
     # if you are not going to train from the very beginning (or only testing),
-    # change this path to the existing model path
+    # change this path to the existing model path (.cpkt file)
     model_path = ''
     beta1 = 0.5
     L1_lambda = 100
     sum_lambda = 0
-    
-    # directory tree setup
-    core_path = "/mnt/ds3lab/dostark"
+
+    ## directory tree setup
+    # 1/ Dataset dependant
+    # Working directory where the project is stored
+    # Default to where this file is stored
+    core_path = os.path.dirname(os.path.abspath(__file__))
     run_case = "%s/z_%s/%s-band" % (core_path, redshift, filter_)
-    ext = '_test_all_stars'
+    ext = '' #Custom extension to differentiate setups
+    # 2/ Precomputation dependant
     stretch_setup = '%s/%s_%s%s' % (run_case, stretch_type, scale_factor, ext)
-    sub_config = '%s/lintrain_classic_WGAN_%s/lr_%s' % (stretch_setup, 
+    # 3/ PSFGAN model dependant
+    sub_config = '%s/lintrain_classic_PSFGAN_%s/lr_%s' % (stretch_setup,
                                                         attention_parameter,
                                                         learning_rate)
-    
-    
-    output_path = '%s/GAN_output' % sub_config
+
+
+    output_path = '%s/PSFGAN_output' % sub_config
     result_path = output_path
     data_path = "%s/npy_input" % stretch_setup
     save_path = "%s/model" % sub_config
-    
+
+    ## Datasets dependant value
+    # This has been precomputed for SDSS datasets
     if '0.01' in run_case:
         pixel_max_value = 41100
     elif '0.05' in run_case:
         pixel_max_value = 6140
     elif '0.1' in run_case:
         pixel_max_value = 1450
-        #pixel_max_value = 483 
     elif '0.2' in run_case:
         pixel_max_value = 1657
-        #pixel_max_value = 271.0
     pixel_min_value = -0.1
-    
-    # contrast distribution
+
+    ## contrast distribution for the added PSF
     max_contrast_ratio = 10
     min_contrast_ratio = 0.1
     uniform_logspace = False
 
-    # training parameters
+    ## training parameters
     # specify which GPU should be used in CUDA_VISIBLE_DEVICES
     use_gpu = 0
     start_epoch = 0
